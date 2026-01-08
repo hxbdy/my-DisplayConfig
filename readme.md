3197212ea7f673f7a930e91b19ef19710404a2d7
Burdock <hxbdy541@gmail.com>
2026-01-08 20:09:20 +0900

    zowieモニタの入力をDPへ切り替える

--- readme.md
+++ readme.md
@@ -2,3 +2,6 @@ DisplayConfig
 https://www.powershellgallery.com/packages/DisplayConfig/5.2.1
 
 https://github.com/MartinGC94/DisplayConfig
+
+モニタの入力切り替えバッチ
+http://www.nirsoft.net/utils/control_my_monitor.html
--- zowie_on.bat
+++ zowie_on.bat
@@ -1,2 +1,4 @@
 @echo off
 powershell -ExecutionPolicy RemoteSigned -File "C:\Users\hxbdy\workspace\Current\DisplayConfig\zowie_on.ps1"
+
+C:\Users\hxbdy\controlmymonitor\ControlMyMonitor.exe  /SetValue "\\.\DISPLAY2\Monitor0" 60 15
