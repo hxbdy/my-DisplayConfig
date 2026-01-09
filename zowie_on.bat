c78b895e3a21bf5c327dbf262417e703db429a10
Burdock <hxbdy541@gmail.com>
2026-01-09 20:08:09 +0900

    短縮モニタIDを使用するように変更

--- zowie_on.bat
+++ zowie_on.bat
@@ -1,4 +1,4 @@
 @echo off
 powershell -ExecutionPolicy RemoteSigned -File "C:\Users\hxbdy\workspace\Current\DisplayConfig\zowie_on.ps1"
 
-C:\Users\hxbdy\controlmymonitor\ControlMyMonitor.exe  /SetValue "\\.\DISPLAY2\Monitor0" 60 15
+C:\Users\hxbdy\controlmymonitor\ControlMyMonitor.exe  /SetValue "BNQ7F83" 60 15
